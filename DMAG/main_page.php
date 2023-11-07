866f2bb8dc2f4623ac534521614daf253f2d25ce
ammarunov <90957477+ammarunov@users.noreply.github.com>
2023-11-07 14:26:45 +0300

    Add files via upload

---
+++ DMAG/main_page.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Главная страница</title>
+    <link rel="stylesheet" type="text/css" href="css/main_page_style.css">
+</head>
+<body>
+    <div id="header">
+        <h1>Header</h1>
+    </div>
+    <div id="wrapper">
+        <!-- <div id="sidebar">
+            <ul>
+                <li><a href="#"><object type="image/svg+xml" data="img/icons/iphone.svg" width="45" height="45"></object>Смартфоны и гаджеты</a></li>
+                <li><a href="#"><object type="image/svg+xml" data="img/icons/laptop.svg" width="45" height="45"></object>Компьютеры и ноутбуки</a></li>
+                <li><a href="#"><object type="image/svg+xml" data="img/icons/display.svg" width="45" height="45"></object>ТВ и мультимедиа</a></li>
+                <li><a href="#"><object type="image/svg+xml" data="img/icons/washing.svg" width="45" height="45"></object>Бытовая техника</a></li>
+                <li><a href="#"><object type="image/svg+xml" data="img/icons/office.svg" width="45" height="45"></object>Товары для офиса</a></li>
+                <li><a href="#"><object type="image/svg+xml" data="img/icons/paint.svg" width="45" height="45"></object>Строительство и ремонт</a></li>
+                <li><a href="#"><object type="image/svg+xml" data="img/icons/sport.svg" width="45" height="45"></object>Спорт и отдых</a></li>
+            </ul>
+        </div> -->
+        <div id="content">
+            <h2>Лучшие новинки</h2>
+            <div id="best-sellers" class="block">
+                <div class="products">
+                    <div class="product">
+                        <img src="img/goods/good1.jpg" alt="Товар 1">
+                        <p>Планшет Lenovo Tab M10 TB-X306X</p>
+                        <p class="price">XXXX,XX ₽</p>
+                    </div>
+                    <div class="product">
+                        <img src="img/goods/good2.jpg" alt="Товар 2">
+                        <p>Материнская плата ASUS PRIME B560M-A</p>
+                        <p class="price">XXXX,XX ₽</p>
+                    </div>
+                    <div class="product">
+                        <img src="img/goods/good3.jpg" alt="Товар 3">
+                        <p>Утюг Philips DST5030/20</p>
+                        <p class="price">XXXX,XX ₽</p>
+                    </div>
+                </div>
+            </div>
+            <h2>Акции</h2>
+            <div id="sales" class="block">
+            <div class="shares">
+                <div class="product">
+                    <img src="img/goods/share1.jpg" alt="Акция 1">                   
+                </div>
+                <div class="product">
+                    <img src="img/goods/share2.jpg" alt="Акция 2">                   
+                </div>
+            </div>                   
+            </div>
+        </div>
+    </div>
+    <div id="footer">
+        <p>&copy; 2023 Footer. Все права защищены.</p>
+    </div>
+</body>
+</html>
+
