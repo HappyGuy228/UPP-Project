7512a80e593a5ec0dedda8cb494849f5ff985886
1n4mat1k <90967147+1n4mat1k@users.noreply.github.com>
2023-12-02 14:30:02 +0300

    Create lang.js

---
+++ DMAG/js/lang.js
@@ -0,0 +1,113 @@
+const langArr = {
+    "header-catalog": {
+        "ru": "Каталог1",
+        "en": "Catalog1",
+    },
+
+    "header-sign_in": {
+        "ru": "Войти",
+        "en": "Sign in",
+    },
+    
+    "header-account": {
+        "ru": "Личный кабинет",
+        "en": "Account",
+    },
+
+    "search-txt": {
+        "ru": "Поиск",
+        "en": "Search",
+    },
+
+    "header-comparsion": {
+        "ru": "Сравнение",
+        "en": "Comparsion",
+    },
+
+    "header-basket": {
+        "ru": "Корзина",
+        "en": "Basket",
+    },
+    
+
+  
+// Страница login.php
+
+    "login-title": {
+        "ru": "Войти",
+        "en": "Sign in",
+    },
+
+    "login-sign_in": {
+        "ru": "Вход",
+        "en": "Sign in",
+    },
+
+    "login-email": {
+        "ru": "Email",
+        "en": "Email",
+    },
+    
+    "login-password": {
+        "ru": "Пароль1",
+        "en": "Password1",
+    },
+
+    "login-button": {
+        "ru": "Войти",
+        "en": "Enter",
+    },
+
+    "register": {
+        "ru": "Регистрация",
+        "en": "Registration",
+    },
+
+
+
+// Страница registration
+  
+    "registration-title": {
+        "ru": "Зарегистрироваться",
+        "en": "Registration",
+    },
+    
+    "registration-registration": {
+        "ru": "Регистрация",
+        "en": "Registration",
+    },
+
+    "registration-name": {
+        "ru": "Имя",
+        "en": "Name",
+    },
+
+    "registration-email": {
+        "ru": "Email",
+        "en": "Email",
+    },
+
+    "registration-password": {
+        "ru": "Пароль",
+        "en": "Password",
+    },
+
+    "registration-accept_password": {
+        "ru": "Подтверждение пароля",
+        "en": "Accept password",
+    },
+
+    "register-button": {
+        "ru": "Зарегистрироваться",
+        "en": "Registrer",
+    },
+
+
+  
+    // Страница trash
+
+    "checkout-btn": {
+        "ru": "Перейти к оформлению",
+        "en": "Go to checkout",
+    },
+}
