f1a8ccde11cddd8561d30d1e12a65b7ba50e8925
1n4mat1k <90967147+1n4mat1k@users.noreply.github.com>
2023-12-05 15:51:35 +0300

    Update trash.php

--- DMAG/trash.php
+++ DMAG/trash.php
@@ -93,7 +93,7 @@
         
     </div>
     
-    <script type='text/javascript' src="js/test_basket1.js"></script>
+    <script type='text/javascript' src="js/test_basket.js"></script>
     <script>
         loadBasket();
     </script>
