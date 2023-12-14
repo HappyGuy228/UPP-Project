574ce8f2651e8ee42bf040d46861e1de2f0d8ff7
1n4mat1k <90967147+1n4mat1k@users.noreply.github.com>
2023-12-14 19:55:20 +0300

    Update test_basket.js

--- DMAG/js/test_basket.js
+++ DMAG/js/test_basket.js
@@ -21,12 +21,81 @@ function addToBasket(val){
 
 function showBasket(){
     var out="";
+    var products="";
+    var amount_elements = 0;
+    
     for (var key in cart) {
-        out += key + ' --- ' + cart[key] + '<br>';
+         out += key + ' --- ' + cart[key] + '<br>';
+        //out += key + ' --- ' + cart[key];
+        ++amount_elements;
+
+        products += 
+            "<!--<div>"+
+            key+
+            "</div> -->"+
+
+        '<div class="product-card" id="product1">'+
+            '<img src="img/trash/product1.jpg" alt="Product 1" class="product-img">'+
+            '<div class="product-info">'+
+                '<div class="product-title">' + key + '</div>'+
+                '<div class="product-description">Очень крутая мышка с продвинутыми функциями.</div>'+
+                '<div class="product-price">7777 руб.</div>'+
+            '</div>'+
+            '<div class="quantity-controls">'+
+                '<button class="quantity-btn" onclick="deleteFromBasket(this.value)" value="' + key + '">-</button>'+
+                '<span class="quantity" id="quantity1">' + cart[key] + '</span>'+
+                '<!--<button class="quantity-btn" onclick="addToBasket(this.value)" value='+ '"Hello world"' +'>+</button>-->'+
+                '<button class="quantity-btn" onclick="addToBasket(this.value)" value="' + key +'">+</button>'+
+                '<!--button class="quantity-btn" onclick="addToBasket(this.value)" value="${key}">+</button> -->'+
+            '</div>'+
+            '<button class="delete-btn" onclick="deleteFromBasketAtAll(this.value)" value="' + key + '">'+
+                '<img src="img/trash/trash-icon.png" alt="Delete" width="20" height="20">'+
+            '</button>'+
+        '</div>';
     }
 
-    $('.basket').html(out);         // Вывод товара на страницу
+    var amount_elements1 = "There " + amount_elements + " elements";
+
+    products += '<button class="checkout-btn" onclick="checkout()">Перейти к оформлению</button>';
+
+    document.getElementById("all_products_in_trash").innerHTML = products;
+    $('.basket2').html(amount_elements1);
+    $('.basket3').html(out);                                    // Можно так
+    //document.getElementById("basket3").innerHTML = out;       // Можно так
+    //$('.product-price').html(out);
 }
+
+// function showBasketElements(){
+//     var out="";
+//     var products = '';
+
+//     for (var key in cart) {
+
+//     products += "<div>"+
+//     "hello11111"+
+//     "</div>";
+
+//         products += '<div class="product-card" id="product1">'+
+//             '<!--<img src="img/trash/product1.jpg" alt="Product 1" class="product-img">-->'+
+//             '<div class="product-info">'+
+//                 '<div class="product-title">Крутая мышка для компьютера 1</div>'+
+//                 '<div class="product-description">Очень крутая мышка с продвинутыми функциями.</div>'+
+//                 '<div class="product-price">7777 руб.</div>'+
+//             '</div>'+
+//             '<div class="quantity-controls">'+
+//                 '<button class="quantity-btn" onclick="deleteFromBasket(this.value)" value="Крутая мышка для компьютера 1">-</button>'+
+//                 '<span class="quantity" id="quantity1">1</span>'+
+//                 '<button class="quantity-btn" onclick="addToBasket(this.value)" value="Крутая мышка для компьютера 1">+</button>'+
+//             '</div>'+
+//             '<button class="delete-btn" onclick="deleteProduct("product1")">'+
+//                 '<!--<img src="img/trash/trash-icon.png" alt="Delete" width="20" height="20">-->'+
+//             '</button>'+
+//         '</div>';
+//     }
+
+//     document.getElementById("all_products_in_trash").innerHTML = products;
+
+// }
 
 function saveBasket(){
     localStorage.setItem('cart', JSON.stringify(cart));
@@ -40,6 +109,7 @@ function loadBasket(){
             localStorage.setItem('cart', JSON.stringify(cart));        
         }    
         showBasket();
+        //showBasketElements();
     }
 }
 
@@ -51,6 +121,14 @@ function deleteFromBasket(val){
         delete(cart[val]);  // если в корзине нет товара, то удаляем его
     }
 
+    console.log(cart);
+    showBasket();
+    saveBasket();
+}
+
+function deleteFromBasketAtAll(val){
+    delete(cart[val]); 
+    
     console.log(cart);
     showBasket();
     saveBasket();
