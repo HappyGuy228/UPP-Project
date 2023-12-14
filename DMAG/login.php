7bedf79c3dcf856c1880192072a3935a18f5172d
1n4mat1k <90967147+1n4mat1k@users.noreply.github.com>
2023-12-14 20:28:01 +0300

    Update login.php

--- DMAG/login.php
+++ DMAG/login.php
@@ -1,19 +1,48 @@
- <?php 
+<?php 
     session_start();
-
     $errors = [];
     if (!empty($_SESSION['user'])){
         header('Location: account.php');
     }
 
-    if (!empty($_POST['email']) && !empty($_POST['password'])){
-        if ($_POST['email'] == '123@mail.com' && $_POST['password'] == '123'){                          // Саня, это тебе: Здесь вместо 123@mail.com и 123 будет запрос в бд для поиска клиента в бд
-            $_SESSION['user'] = ['email' => $_POST['email'], 'password' => $_POST['password']];         // Занесение информации о пароле и почте в сессию
-            header('Location: account.php');
-            die;
+    if(isset($_POST['login-button'])){
+        if ($_POST['login-email'] == '' || $_POST['input-login-password'] == ''){
+            $errors[] = 'Вы ввели не все данные';
         }
         else{
-            $errors[] = 'Неверный логин или пароль';
+            $db_host = 'localhost';
+            $db_user = 'root';
+            $db_pass = ''; 
+            $db_name = 'DMAG';
+
+            $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+            if (!$conn) {
+                die('Ошибка подключения к базе данных:' . mysqli_connect_error());
+            }
+            else{
+                echo "Есть подключение к БД <br />";
+            }
+
+            $Email = $_POST['login-email'];
+            $Password = $_POST['input-login-password'];
+            $md5pass = md5($Password);
+
+            $query = "SELECT * FROM user where email = '$Email' AND password = '$md5pass'";
+
+            $result = $conn->query($query);
+            if ($result->num_rows == 1) {
+
+                $data_from_db = $result->fetch_assoc();
+                $Role = $data_from_db["role"];
+                $Name = $data_from_db["name"];
+                $_SESSION['user'] = ['email' => $_POST['email'], 'role' => $Role, 'name' => $Name];
+                header('Location: main_window.php');
+                
+            } else {
+                $errors[] = 'Введен неверный email или пароль!';
+            }
+            $conn->close(); 
         }
     }
 ?>
